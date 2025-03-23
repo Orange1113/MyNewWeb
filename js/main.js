9f76ed255801f757a181941241b36f7058293064
Orange1113 <sh_wjc_2013@126.com>
2025-03-23 13:16:33 +0800

    (1.2.3)feat: 添加IE浏览器检测
    检测用户是否使用IE浏览器，若检测到则跳转至不支持页面。同时移除了部分未使用的CSS样式，优化了页面加载逻辑。

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
+// 检测IE浏览器并跳转
+function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+    
+    if (msie > 0 || trident > 0) {
+        window.location.href = '/unsupported.html';
+        return true;
+    }
+    return false;
+}
+
 $(document).ready(function() {
+    // 检测IE浏览器
+    if (detectIE()) return;
+
     // 加载并插入 header.html
     $.get('template/header.html', function(data) {
         $('body').prepend(data);
         
         // 获取页面标题并更新导航栏中的 span 元素
         var pageTitle = document.title;
-        document.title = '橙子113 - ' + pageTitle; // 修改页面标题
+        document.title = '橙子113 - ' + pageTitle;
         $('#page-title').text(' - ' + pageTitle);
     });
 
